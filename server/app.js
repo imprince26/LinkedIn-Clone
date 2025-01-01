a2c92f8a42c34cddd6d9bce3329b519ef7253e2a
imprince26 <princepatel260806@gmail.com>
2025-01-01 18:31:45 +0530

    update files

--- server/app.js
+++ server/app.js
@@ -19,12 +19,11 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(
-		cors({
-			origin: process.env.CLIENT_URL || "http://localhost:5173",
-			credentials: true,
-		})
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
 );
-
 
 app.use(express.json()); // parse JSON request bodies
 app.use(cookieParser());
@@ -36,14 +35,14 @@ app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-	connectDB();
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -103,10 +103,10 @@ export const logout = (req, res) => {
 };
 
 export const getCurrentUser = async (req, res) => {
-  try {
-    const resp = await res.json(req.user);
-  } catch (error) {
-    console.error("Error in getCurrentUser controller:", error);
-    res.status(500).json({ message: "Server error" });
-  }
+	try {
+		res.json(req.user);
+	} catch (error) {
+		console.error("Error in getCurrentUser controller:", error);
+		res.status(500).json({ message: "Server error" });
+	}
 };
