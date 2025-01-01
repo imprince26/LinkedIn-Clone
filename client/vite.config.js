8d3d5b16bd30ff96968bc92bf0f72bcec548e2e8
imprince26 <princepatel260806@gmail.com>
2025-01-01 18:48:03 +0530

    update files

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    VITE_BACKEND_URL:"https://linkedin-server-a773.onrender.com/api/v1" 
-  }
+  plugins: [react()]
 })
