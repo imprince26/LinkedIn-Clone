0c91942519501404cfdab7d97026f77ff4ba34e0
imprince26 <princepatel260806@gmail.com>
2025-01-06 22:48:21 +0530

    # update files

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,4 +3,8 @@ import axios from "axios";
 export const axiosInstance = axios.create({
 	baseURL:`${import.meta.env.VITE_BACKEND_URL}`,
 	withCredentials: true,
+	headers: {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	  }
 });
--- server/app.js
+++ server/app.js
@@ -22,6 +22,13 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type', 
+      'Authorization', 
+      'Access-Control-Allow-Credentials',
+      'Cookie'
+    ]
   })
 );
 
