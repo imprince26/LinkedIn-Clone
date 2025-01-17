9d8c2cbe62493a9fb8aa7d6714682fa97d20342c
imprince26 <princepatel260806@gmail.com>
2025-01-17 23:22:33 +0530

    update code

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { axiosInstance } from "../lib/axios";
